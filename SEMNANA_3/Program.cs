08de90b22b4b47f6ee0af38c996b99ed9df01de6
BODOQUE <Epaytan4@gmail.com>
2025-09-02 12:02:53 -0500

    siu

---
+++ SEMNANA_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMNANA_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+        }
+        static void ejer1()
+        {
+            Console.WriteLine("NOMBRE: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("CARRERA: ");
+            string carrera = Console.ReadLine();
+            Console.WriteLine($"\n {nombre}  bienvenido a mundo UPN en su carrera de  { carrera}  :v");
+            
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ SEM_3 C# PERO PYTON/SEM_3_C__PERO_PYTON.py
@@ -0,0 +1,6 @@
+def ejer1():
+    nombre= input ("NOMBRE: ")
+    carrera = input("CARRERA: ")
+    print (f"\n {nombre}  bienvenido a mundo UPN en su carrera de  { carrera}  :v py")
+
+ejer1()
